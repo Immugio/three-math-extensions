ed66775c33e961835b23843222b822cfd9c16b1d
Immugio <janmikeska@gmail.com>
2023-01-02 13:07:21 +0100

    Add Polygon to exports

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,5 @@ export { Vec2 } from "./Vec2";
 export { Vec3 } from "./Vec3";
 export { Line2D } from "./Line2D";
 export { Line3D } from "./Line3D";
+export { Size2 } from "./Size2";
+export { Polygon } from "./Polygon";
